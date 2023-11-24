45b8b7cfee9904c313f60d8d71ffec6b54849dcc
Djordje <djdjoko@gmail.com>
2023-11-24 13:46:29 +0100

    added run.bat

---
+++ run.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "C:\Users\Djordje\miniconda3\envs\uni\python.exe main.py --category-groups-list-filter * --country-code-filter "USA" --crunchbase-run true"
+
+start cmd.exe /k "C:\Users\Djordje\miniconda3\envs\uni\python.exe main.py --category-groups-list-filter * --country-code-filter * --companieshouse-run true"
